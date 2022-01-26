8df1b91f7b7accad81f058a2c429d0e79b341a8b
JoDamaso <darryldamaso@gmail.com>
2022-01-26 16:45:23 -0600

    fixed list issues

--- module-6-starter/assets/js/homepage.js
+++ module-6-starter/assets/js/homepage.js
@@ -36,8 +36,9 @@ var displayRepos = function(repos, searchTerm) {
         var repoName = repos[i].owner.login + "/" + repos[i].name;
 
         //create a container for each repo
-        var repoEl = document.createElement("div");
+        var repoEl = document.createElement("a"); // creates an a tag
         repoEl.classList = "list-item flex-row justify-space-between align-center";
+        repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName); // has id and sends us to the HTML issue page
 
         //create a span element to hold repository name
         var titleEl = document.createElement("span");
--- module-6-starter/assets/js/single.js
+++ module-6-starter/assets/js/single.js
@@ -1,23 +1,46 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
 
+var getRepoName = function () {
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
 
-var getRepoIssues = function(repo) {
+    if (repoName) { // checks if the repo name acutally exists
+        // display repo name on page
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    }
+    else {
+        //sends user back to homepage if no repo was given
+        document.location.replace("./index.html");
+    }
+};
+
+var getRepoIssues = function (repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
-    fetch(apiUrl).then(function(response) {
+    fetch(apiUrl).then(function (response) {
         // if request was successful
         if (response.ok) {
-            response.json().then(function(data) {
+            response.json().then(function (data) {
                 // pass response data to DOM function
                 displayIssues(data);
+
+                // check if api has paginated issues
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             });
         }
         else {
-            alert("There was a problem with your request!");
+            //if not successful, redirect to homepage
+            document.location.replace("./index.html");
         }
     });
 };
 
-var displayIssues = function(issues) {
+var displayIssues = function (issues) {
     // checking if the repo has no open issues
     if (issues.length === 0) {
         issueContainerEl.textContent = "This repo has no open issues!"
@@ -56,5 +79,17 @@ var displayIssues = function(issues) {
     }
 };
 
+var displayWarning = function (repo) {
+    //add text to warning container 
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
 
-getRepoIssues("jodamaso/taskinator");
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See more issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank"); // when clicked, will open new tab
+
+    //append created linkEl a to DOM element on the HTML
+    limitWarningEl.appendChild(linkEl);
+};
+
+getRepoName();
